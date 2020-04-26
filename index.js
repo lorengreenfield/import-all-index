80b7497a6a026014e70c1e627e548e5134f58e49
lorengreenfield <loren@intereact.co.nz>
2020-04-27 10:04:40 +1200

    Convert windows paths to file urls

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ export default function importAllIndex (dir) {
     for (let file of files) {
       let filePath = path.join(dir, file)
       if (fs.statSync(filePath).isDirectory()) {
-        let childImport = await importAllIndex(url.pathToFileURL(filePath).toString())
+        let childImport = await importAllIndex(filePath)
         imported = imported.concat(childImport)
       } else if (file === 'index.mjs' || file === 'index.js') {
-        let imp = await import(filePath)
+        let imp = await import(url.pathToFileURL(filePath).toString())
         imported.push(imp)
       }
     }
